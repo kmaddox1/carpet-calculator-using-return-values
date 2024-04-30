ff6318ae83705bfc8a85a643010b6f654b4e0e33
Kimberly Maddox <160989452+kmaddox1@users.noreply.github.com>
2024-04-30 22:48:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// square footage = length * width
+
+// Write a function that takes in a length and width and returns the square footage of
+
+// keyword "let" is used to declare a variable explained in the next section.
+
+// prompt function is used to get user input (values). then we put some words ("") in the prompt function to tell the user what to do.
+// sqFt is sqaure footage.
+
+
+let length = prompt("Room leghth in feet");
+let width = prompt("Room width in feet");
+let sqFt = length * width;
+
+alert(sqFt);
+  
